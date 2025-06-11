eae139b611757cb4387e2afdd0395844a6430b23
kaanyalti <kaan.yalti@elastic.co>
2025-06-11 19:58:29 -0400

    fix(7794): add elastic-agent group to socket when running unprivileged as admin

---
+++ pkg/core/constants/constants.go
@@ -0,0 +1,12 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
+
+package constants
+
+const (
+	// ElasticUsername is the default username used when running Elastic Agent in unprivileged mode
+	ElasticUsername = "elastic-agent-user"
+	// ElasticGroupName is the default group name used when running Elastic Agent in unprivileged mode
+	ElasticGroupName = "elastic-agent"
+)
